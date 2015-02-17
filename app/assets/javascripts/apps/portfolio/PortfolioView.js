4e4f683d34c43a66edf5b840bf64d8cb6ee0a726
Mario Hayashi <mario.hayashi@gmail.com>
2015-02-17 21:39:15 +0000

    Add total portfolio value (sum of all positions) to portfolios view

--- app/assets/javascripts/apps/portfolio/PortfolioView.js
+++ app/assets/javascripts/apps/portfolio/PortfolioView.js
@@ -9,12 +9,25 @@ App.module("Portfolio", function (Portfolio, App, Backbone, Marionette, $, _) {
             'click .edit-portfolio': 'onClickEditPortfolio',
             'click .delete-portfolio': 'onClickDeletePortfolio'
         },
+        initialize: function () {
+            this.positions = new Portfolio.Position.Collection();
+            this.positions.portfolio_id = this.model.get('id');
+            this.listenToOnce(this.positions, 'sync', this.onSyncPortfolioPositions);
+            this.positions.fetch();
+        },
         onClickEditPortfolio: function (event) {
             var url = this.model.get('url').replace('.json','/edit');
             window.location.href = url;
         },
         onClickDeletePortfolio: function (event) {
             this.model.destroy();
+        },
+        onSyncPortfolioPositions: function (positions) {
+            // Value of portfolio is sum of all its positions.
+            var totalValue = positions.reduce(function (memo, position) {
+                    return memo + position.get('units') * position.get('current_price');
+                }, 0);
+            this.$('.total-value').text(totalValue);
         }
     });
 });
